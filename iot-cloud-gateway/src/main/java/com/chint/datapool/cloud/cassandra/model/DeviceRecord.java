e5077ed87c2341f715ea39ebf20043582ceb45b2
Darcty <shdk_ws@chint.com>
2019-01-08 09:49:48 +0800

    提交还原

--- iot-cloud-gateway/src/main/java/com/chint/datapool/cloud/cassandra/model/DeviceRecord.java
+++ iot-cloud-gateway/src/main/java/com/chint/datapool/cloud/cassandra/model/DeviceRecord.java
@@ -1,68 +1,68 @@
-//package com.chint.datapool.cloud.cassandra.model;
-//
-//import java.util.Date;
-//import java.util.UUID;
-//import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-//import org.springframework.data.cassandra.core.mapping.Table;
-//
-//
-////设备历史记录
-//@Table
-//public class DeviceRecord   {
-//	@PrimaryKey
-//	private UUID id;
-//
-//	private UUID deviceId;
-//	
-//	private Date uploadTime;
-//
-//	private String property;
-//
-//	public DeviceRecord() {
-//	}
-//
-//	public DeviceRecord(UUID id, UUID deviceId, Date uploadTime, String property) {
-//		this.id = id;
-//		this.deviceId = deviceId;
-//		this.uploadTime = uploadTime;
-//		this.property = property;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return String.format("DeviceRecord[id=%s, deviceId=%s, uploadTime='%s', property='%s']", 
-//				this.id,this.deviceId,this.uploadTime, this.property);
-//	}
-//
-//	public UUID getId() {
-//		return id;
-//	}
-//
-//	public void setId(UUID id) {
-//		this.id = id;
-//	}
-//
-//	public UUID getDeviceId() {
-//		return deviceId;
-//	}
-//
-//	public void setDeviceId(UUID deviceId) {
-//		this.deviceId = deviceId;
-//	}
-//
-//	public Date getUploadTime() {
-//		return uploadTime;
-//	}
-//
-//	public void setUploadTime(Date uploadTime) {
-//		this.uploadTime = uploadTime;
-//	}
-//
-//	public String getProperty() {
-//		return property;
-//	}
-//
-//	public void setProperty(String property) {
-//		this.property = property;
-//	}
-//}
+package com.chint.datapool.cloud.cassandra.model;
+
+import java.util.Date;
+import java.util.UUID;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+
+//设备历史记录
+@Table
+public class DeviceRecord   {
+	@PrimaryKey
+	private UUID id;
+
+	private UUID deviceId;
+	
+	private Date uploadTime;
+
+	private String property;
+
+	public DeviceRecord() {
+	}
+
+	public DeviceRecord(UUID id, UUID deviceId, Date uploadTime, String property) {
+		this.id = id;
+		this.deviceId = deviceId;
+		this.uploadTime = uploadTime;
+		this.property = property;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("DeviceRecord[id=%s, deviceId=%s, uploadTime='%s', property='%s']", 
+				this.id,this.deviceId,this.uploadTime, this.property);
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public UUID getDeviceId() {
+		return deviceId;
+	}
+
+	public void setDeviceId(UUID deviceId) {
+		this.deviceId = deviceId;
+	}
+
+	public Date getUploadTime() {
+		return uploadTime;
+	}
+
+	public void setUploadTime(Date uploadTime) {
+		this.uploadTime = uploadTime;
+	}
+
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+}
--- iot-cloud-gateway/src/main/java/com/chint/datapool/cloud/cassandra/model/PressureGage.java
+++ iot-cloud-gateway/src/main/java/com/chint/datapool/cloud/cassandra/model/PressureGage.java
@@ -1,80 +1,80 @@
-//package com.chint.datapool.cloud.cassandra.model;
-//
-//import java.util.Date;
-//import java.util.UUID;
-//import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-//import org.springframework.data.cassandra.core.mapping.Table;
-//
-//
-////压力表
-//@Table
-//public class PressureGage implements Sensor {
-//	@PrimaryKey
-//	private UUID id;
-//
-//	private String companyName;
-//	
-//	private String deviceName;
-//	
-//	private String description;
-//		
-//	private Date registerTime;
-//
-//	
-//	public PressureGage() {
-//	}
-//
-//	public PressureGage(UUID id, String companyName, String deviceName, String description, Date registerTime) {
-//		this.id = id;
-//		this.companyName = companyName;
-//		this.deviceName = deviceName;
-//		this.description = description;
-//		this.registerTime = registerTime;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return String.format("PressuraGage[id=%s, companyName='%s', deviceName='%s', description='%s', registerTime='%s']", 
-//				this.id,this.companyName, this.deviceName,this.description, this.registerTime);
-//	}
-//
-//	public UUID getId() {
-//		return id;
-//	}
-//
-//	public void setId(UUID id) {
-//		this.id = id;
-//	}
-//
-//	public String getCompanyName() {
-//		return companyName;
-//	}
-//
-//	public void setCompanyName(String companyName) {
-//		this.companyName = companyName;
-//	}
-//
-//	public String getDeviceName() {
-//		return deviceName;
-//	}
-//
-//	public void setDeviceName(String deviceName) {
-//		this.deviceName = deviceName;
-//	}
-//
-//	public String getDescription() {
-//		return description;
-//	}
-//
-//	public void setDescription(String description) {
-//		this.description = description;
-//	}
-//
-//	public Date getRegisterTime() {
-//		return registerTime;
-//	}
-//
-//	public void setRegisterTime(Date registerTime) {
-//		this.registerTime = registerTime;
-//	}
-//}
+package com.chint.datapool.cloud.cassandra.model;
+
+import java.util.Date;
+import java.util.UUID;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+
+//压力表
+@Table
+public class PressureGage implements Sensor {
+	@PrimaryKey
+	private UUID id;
+
+	private String companyName;
+	
+	private String deviceName;
+	
+	private String description;
+		
+	private Date registerTime;
+
+	
+	public PressureGage() {
+	}
+
+	public PressureGage(UUID id, String companyName, String deviceName, String description, Date registerTime) {
+		this.id = id;
+		this.companyName = companyName;
+		this.deviceName = deviceName;
+		this.description = description;
+		this.registerTime = registerTime;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("PressuraGage[id=%s, companyName='%s', deviceName='%s', description='%s', registerTime='%s']", 
+				this.id,this.companyName, this.deviceName,this.description, this.registerTime);
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getDeviceName() {
+		return deviceName;
+	}
+
+	public void setDeviceName(String deviceName) {
+		this.deviceName = deviceName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getRegisterTime() {
+		return registerTime;
+	}
+
+	public void setRegisterTime(Date registerTime) {
+		this.registerTime = registerTime;
+	}
+}
