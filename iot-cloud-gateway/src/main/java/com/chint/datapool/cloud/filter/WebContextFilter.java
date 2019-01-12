3ace8e3c78518dca62e3ffb6b44859abc0cad553
Darcty <shdk_ws@chint.com>
2019-01-12 13:42:54 +0800

    java服务端允许跨越请求

--- iot-cloud-gateway/src/main/java/com/chint/datapool/cloud/filter/WebContextFilter.java
+++ iot-cloud-gateway/src/main/java/com/chint/datapool/cloud/filter/WebContextFilter.java
@@ -17,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * 
  * @ClassName:  WebContextFilter   
- * @Description:跨域访问(sosoApi),自定义的header要加入   
+ * @Description:跨域访问(sosoApi),自定义的header要添加,@CrossOrigin
  * @author: wusheng
  * @date:   2019年1月11日 上午8:32:33   
  *
@@ -36,13 +36,13 @@ public class WebContextFilter implements Filter {
     		FilterChain chain) throws IOException, ServletException {
     	HttpServletResponse resp = (HttpServletResponse)response;
     	//"*"存在风险，建议指定可信任的域名来接收响应信息，如"http://www.sosoapi.com"
-    	resp.addHeader("Access-Control-Allow-Origin", "http://www.sosoapi.com");
+    	resp.addHeader("Access-Control-Allow-Origin", "*");
     	//如果存在自定义的header参数，需要在此处添加，逗号分隔
-    	resp.addHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, "
-    			+ "If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, "
+    	resp.addHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With,"
+    			+ "If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires,"
     			+ "Content-Type, X-E4M-With,"
     			+ "Authorization");
-    	resp.addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");  
+    	resp.addHeader("Access-Control-Allow-Methods", "OPTIONS,GET,POST,DELETE,PUT");  
     	resp.addHeader("Access-Control-Allow-Credentials", "true");
     	
     	chain.doFilter(request, response);
